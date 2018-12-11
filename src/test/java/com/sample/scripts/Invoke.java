51482dc74881414a82ab52050047aaa9acb9ea74
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2018-12-11 21:26:15 +0530

    automation9

--- src/test/java/com/sample/scripts/Invoke.java
+++ src/test/java/com/sample/scripts/Invoke.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class Invoke {
-	
+
 	@Test
 	public void invokeApp(){
 		/*System.setProperty("webdriver.chrome.driver", "E:\\Jenkin_Dec06\\sample\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();*/
 		System.out.println(System.getProperty("user.dir"));
 		System.setProperty("webdriver.gecko.driver",  System.getProperty("user.dir")+"/Drivers/geckodriver");
+		System.setProperty("DISPLAY", ":0");
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", true);
 		WebDriver driver = new FirefoxDriver(capabilities);
